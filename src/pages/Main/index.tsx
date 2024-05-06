b7868b825076b64db83423bbbd41afef944a169c
Alexandre de Lima Konrath <160286787+Alexandre-Konrath@users.noreply.github.com>
2024-05-06 10:04:37 -0300

    adding the footer component to the main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,18 @@
-import { Sidebar } from '../../components/Sidebar'
+import { Outlet } from 'react-router-dom'
 
+import { Sidebar } from '../../components/Sidebar'
 import { Container } from './styles'
+
+import LogoImg from '../../assets/img/logo.svg'
 
 export default function Main() {
   return (
     <Container>
       <Sidebar />
-      <h1>main</h1>
+      <section>
+        <img src={LogoImg} alt='Imagem logo' />
+        <Outlet /> {/* indicador de sub paginas */}
+      </section>
     </Container>
   )
 }
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -5,4 +5,29 @@ export const Container = styled.main`
   height: 100vh;
 
   display: flex;
+  > section { // adiciona o estilo somente pra section imediata, sem ser a que está no componente
+    width: 100%;
+    height: 100vh;
+
+    overflow-y: auto;
+
+    flex: 1;
+    padding: 2rem 1.875rem;
+
+    img {
+      width: 10rem;
+      margin-bottom: 2rem;
+    }
+
+    @media (max-width: 720px) {
+      display: flex;
+      flex-direction: column;
+
+      padding-bottom: 8rem;
+
+      img {
+        align-self: center;
+      }
+    }
+  }
 `
