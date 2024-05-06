a4706b215a4cae81bc2f8de5caf5e4f4ae03cde8
Alexandre de Lima Konrath <160286787+Alexandre-Konrath@users.noreply.github.com>
2024-05-06 10:12:22 -0300

    Title component that changes its name dynamically depending on the page you are on

---
+++ src/components/SnackTitle/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  display: flex;
+  align-items: center;
+
+  text-transform: uppercase;
+
+  gap: 0.5rem;
+  margin-bottom: 3rem;
+  font-weight: 500;
+
+  font-size: ${({ theme }) => theme.fontSize["4xl"]}; // 2.75 x 16 = 44 === '4xl': rem(40),
+  &::before {
+    content: '';
+    background: ${({ theme }) => theme.colors.white};
+    width: 35px;
+    height: 5px;
+    flex-shrink: 0;
+    border-radius: 4px;
+  }
+
+  @media (max-width: 720px) {
+    font-size: ${({ theme }) => theme.fontSize["3xl"]}; // 2 x 16 = 32 === '3xl': rem(32),
+    justify-content: center;
+
+    &::before {
+      display: none;
+    }
+  }
+`;
