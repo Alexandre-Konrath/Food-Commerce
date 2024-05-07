c4d55445fa04fa2296348fd4cce4801ad00809e6
Alexandre de Lima Konrath <160286787+Alexandre-Konrath@users.noreply.github.com>
2024-05-07 09:33:13 -0300

    adding the context of snacks to embrace everyone

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { AppRoutes } from './routes'
 
-import { Theme } from './styles/Theme'
+import { SnackProvider } from './contexts/SnackContext'
+
 import { GlobalStyle } from './styles/global'
+import { Theme } from './styles/Theme'
 import { Normalize } from 'styled-normalize'
 
 export default function App() {
+
   return (
     <BrowserRouter>
       <Theme>
-        <AppRoutes />
-        <GlobalStyle />
-        <Normalize />
+        <SnackProvider>
+          <AppRoutes />
+          <GlobalStyle />
+          <Normalize />
+        </SnackProvider>
       </Theme>
     </BrowserRouter>
   )
