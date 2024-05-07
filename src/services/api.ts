c90fd0643363cad52e524ae6a39d5deefe47d665
Alexandre de Lima Konrath <160286787+Alexandre-Konrath@users.noreply.github.com>
2024-05-07 09:34:54 -0300

    creating a small api that returns the data for each snack with a separate route

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,14 @@
 import axios from 'axios'
+import { SnackData } from '../interfaces/snackData'
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 })
+// <SnackData[]> = se não carregar o que esperamos = snacks, ele retorna um erro do typeScript
+export const getBurgers = () => api.get<SnackData[]>('/burgers')
+export const getDrinks = () => api.get<SnackData[]>('/drinks')
+export const getIceCreams = () => api.get<SnackData[]>('/ice-creams')
+export const getPizzas = () => api.get<SnackData[]>('/pizzas')
+
 
 export default api
